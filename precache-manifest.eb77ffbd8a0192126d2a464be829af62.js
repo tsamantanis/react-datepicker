f51bd0a5e5f7ebe03734e078677b72d5c93ff63f
tsamantanis <philip.tsamantanis@icloud.com>
2021-10-05 10:36:38 -0700

    Updates

---
+++ precache-manifest.eb77ffbd8a0192126d2a464be829af62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644be76b7386622419d69bac268745ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1bd3a89919389730107",
+    "url": "./static/css/main.1c856862.chunk.css"
+  },
+  {
+    "revision": "6d3be463380b3a6d4ecc",
+    "url": "./static/js/2.ccf5af93.chunk.js"
+  },
+  {
+    "revision": "20108172ed757122913e57d7a75ecb65",
+    "url": "./static/js/2.ccf5af93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bd3a89919389730107",
+    "url": "./static/js/main.6c532be6.chunk.js"
+  },
+  {
+    "revision": "a7bf64704675b3a9db04",
+    "url": "./static/js/runtime-main.631d03f9.js"
+  }
+]);
