94e3847616911b8ed28102fb69498b6faabc175a
tsamantanis <philip.tsamantanis@icloud.com>
2021-10-15 07:27:03 -0700

    Format example page

--- example/src/App.js
+++ example/src/App.js
@@ -6,46 +6,54 @@ import '@tsamantanis/react-datepicker/dist/index.css'
 const App = () => {
   return (
     <>
-      <DatePicker
-        timezone="America/Los_Angeles" // Check moment-timezone documentation
-        disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
-        multiple={3} // (Optional) Number of dates to be selected
-        consecutive={2} // (Optional) Number of consecutive days to appear selected
-        onDayClick={(selectedDates) => console.log(selectedDates)}
-        onMonthChange={(month) => console.log(month)}
-      />
-      <DatePicker
-        timezone="America/Los_Angeles" // Check moment-timezone documentation
-        changeYear={true} // Boolean for allowing users to change calendar years
-        onDayClick={(selectedDates) => console.log(selectedDates)}
-        onMonthChange={(month) => console.log(month)}
-        onYearChange={(year) => console.log(year)}
-      />
-      <DatePicker
-        timezone="America/Los_Angeles" // Check moment-timezone documentation
-        disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
-        multiple={3} // (Optional) Number of dates to be selected
-        consecutive={2} // (Optional) Number of consecutive days to appear selected
-        onDayClick={(selectedDates) => console.log(selectedDates)}
-        onMonthChange={(month) => console.log(month)}
-        textColor="#ffffff"
-        backgroundColor="#14aeff"
-        borderRadius="10px"
-        boxShadow="none"
-      />
-      <DatePicker
-        timezone="America/Los_Angeles" // Check moment-timezone documentation
-        disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
-        multiple={3} // (Optional) Number of dates to be selected
-        consecutive={2} // (Optional) Number of consecutive days to appear selected
-        onDayClick={(selectedDates) => console.log(selectedDates)}
-        onMonthChange={(month) => console.log(month)}
-        fontFamily="'VT323', monospace"
-        textColor="#000000"
-        backgroundColor="#F9EFE4"
-        borderRadius="0px"
-        boxShadow="4px 4px #C8C2BA"
-      />
+      <div className="calendarContainer default">
+        <DatePicker
+          timezone="America/Los_Angeles" // Check moment-timezone documentation
+          disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
+          multiple={3} // (Optional) Number of dates to be selected
+          consecutive={2} // (Optional) Number of consecutive days to appear selected
+          onDayClick={(selectedDates) => console.log(selectedDates)}
+          onMonthChange={(month) => console.log(month)}
+        />
+      </div>
+      <div className="calendarContainer year">
+        <DatePicker
+          timezone="America/Los_Angeles" // Check moment-timezone documentation
+          changeYear={true} // Boolean for allowing users to change calendar years
+          onDayClick={(selectedDates) => console.log(selectedDates)}
+          onMonthChange={(month) => console.log(month)}
+          onYearChange={(year) => console.log(year)}
+        />
+      </div>
+      <div className="calendarContainer">
+        <DatePicker
+          timezone="America/Los_Angeles" // Check moment-timezone documentation
+          disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
+          multiple={3} // (Optional) Number of dates to be selected
+          consecutive={2} // (Optional) Number of consecutive days to appear selected
+          onDayClick={(selectedDates) => console.log(selectedDates)}
+          onMonthChange={(month) => console.log(month)}
+          textColor="#ffffff"
+          backgroundColor="#14aeff"
+          borderRadius="10px"
+          boxShadow="none"
+        />
+      </div>
+      <div className="calendarContainer retro">
+        <DatePicker
+          timezone="America/Los_Angeles" // Check moment-timezone documentation
+          disabled={["2021-10-09", "2021-10-10", "2021-10-19"]} // (Optional) Array of YYYY-MM-DD formatted dates to appear disabled
+          multiple={3} // (Optional) Number of dates to be selected
+          consecutive={2} // (Optional) Number of consecutive days to appear selected
+          onDayClick={(selectedDates) => console.log(selectedDates)}
+          onMonthChange={(month) => console.log(month)}
+          fontFamily="'VT323', monospace"
+          textColor="#000000"
+          backgroundColor="#F9EFE4"
+          borderRadius="0px"
+          boxShadow="4px 4px #C8C2BA"
+        />
+      </div>
     </>
   )
 }
